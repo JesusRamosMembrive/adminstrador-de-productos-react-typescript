863931053f6d94e792662c64c6cfd1e1194c86cf
JesusRamos <jesusramos@overture.life>
2024-12-25 09:57:52 +0100

    add new content

--- rest_api_nodejs_server/src/server.ts
+++ rest_api_nodejs_server/src/server.ts
@@ -1,25 +1,30 @@
 import express from 'express';
 import router from './router';
+// @ts-ignore
 import db from '../config/db';
+import colors from 'colors';
+
 
 // Conectar base de datos
-
 async function connectDB() {
     try {
         await db.authenticate();
         await db.sync();
-        console.log('Connection has been established successfully.');
+        console.log(colors.blue.bold('Connection has been established successfully.'));
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        console.error(colors.red.bold('Unable to connect to the database:'), error);
     }
 }
 
-connectDB();
+connectDB().then(r => console.log('DB connected'));
 
 
 const server = express();
 
-server.use("/", router);
+// Leer datos de formularios
+server.use(express.json());
+
+server.use("/api/products", router);
 
 
 export default server
